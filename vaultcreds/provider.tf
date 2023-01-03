f7534988040dc47e4fc05c9b1dcd46d52eacefe3
maheshctrl <mahesh881425@gmail.com>
2023-01-03 11:48:30 +0530

    integration completed

---
+++ vaultcreds/provider.tf
@@ -0,0 +1,9 @@
+provider "vault" {
+  address = var.vault_address
+  token   = data.aws_ssm_parameter.token.value
+}
+
+provider "aws" {
+  #profile    = root  
+  region = var.region
+}
---
+++ vaultcreds/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+    description = "AWS region"
+    type= string   
+  
+}
+variable "vault_address" {
+    description = "URL address for vault endpoint"
+    type = string
+  
+}
